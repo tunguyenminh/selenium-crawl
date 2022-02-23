14e4092da66a2b0d2a3917f45f518978ef9dbda4
tunguyenminh <tunguyenminh12369@gmail.com>
2022-02-24 02:26:39 +0700

    update see next

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const webdriver = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const chromedriver = require("chromedriver");
 const fs = require("fs");
+const { createDecipher } = require("crypto");
 
 (async function helloSelenium() {
   const options = new chrome.Options();
@@ -15,13 +16,19 @@ const fs = require("fs");
     .build();
 
   // đăng nhập
-  await driver.get("https://m.facebook.com");
+  await driver.get("https://facebook.com");
   await driver
-    .findElement(webdriver.By.id("m_login_email"))
+    .findElement(webdriver.By.id("email"))
     .sendKeys("tunguyenminh12369@gmail.com");
-  await driver.findElement(webdriver.By.id("m_login_password")).sendKeys("220699Aa");
-  await driver.findElement(webdriver.By.id("m_login_password")).submit();
+  await driver.findElement(webdriver.By.id("pass")).sendKeys("220699a");
+  await driver.findElement(webdriver.By.xpath("/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button")).submit();
   await driver.sleep(5000);
+  // login by cookie 
+  // const cookie = "cookie: datr=4lQWYhR1tdiqvypd6il3zEOX; sb=4lQWYiNQ2RWwvjrYYFHOPOV7; m_pixel_ratio=1; locale=vi_VN; c_user=100013017486715; xs=16%3ACqRwPme1h8s4_A%3A2%3A1645630921%3A-1%3A12649; wd=1348x924; fr=0MTRaslsTkKdxWY9J.AWXF6BqU4VWdEtmlcFye3vljiuc.BiFlTi.7Y.AAA.0.0.BiFlXM.AWUbk3a7XjY"
+  // let script = 'javascript:void(function(){ function setCookie(t) { var list = t.split("; "); console.log(list); for (var i = list.length - 1; i >= 0; i--) { var cname = list[i].split("=")[0]; var cvalue = list[i].split("=")[1]; var d = new Date(); d.setTime(d.getTime() + (7*24*60*60*1000)); var expires = ";domain=.facebook.com;expires="+ d.toUTCString(); document.cookie = cname + "=" + cvalue + "; " + expires; } } function hex2a(hex) { var str = ""; for (var i = 0; i < hex.length; i += 2) { var v = parseInt(hex.substr(i, 2), 16); if (v) str += String.fromCharCode(v); } return str; } setCookie("' + cookie + '"); location.href = "https://facebook.com"; })();'
+  // driver.executeScript(script)
+
+  
   // await driver.findElement(webdriver.By.xpath("/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div/div[3]/form/div[5]/div[1]/button")).click();
 
   // lấy bài viết thay link
@@ -33,6 +40,7 @@ const fs = require("fs");
   // await driver.findElement(webdriver.By.xpath("/html/body/div[1]/div/div[3]/div[1]/div/div/div[2]/div/div/div[1]/i")).click()
   // await driver.findElement(webdriver.By.xpath("/html/body/div[5]")).click();
   await driver.sleep(2000);
+  let index = 0
   while (true) {
     await driver.sleep(2000);
     // thay xpath cần check (optional)
@@ -42,48 +50,58 @@ const fs = require("fs");
     driver.executeScript("arguments[0].scrollIntoView();", loadMoreComment);
     await driver.findElement(webdriver.By.xpath(xPath)).click();
     await driver.sleep(1000);
-
+    // const closeButton = await driver.findElement(webdriver.By.id("popup_xout"))
+    // if(closeButton)
+    //   closeButton.click()
     // check đã lấy hết comment
 
-    if (loadMoreComment.length()){
-        loadMoreComment[0].click()
-      }
-    else{break}
-        
-    // try {
-    //   // thay xpath cần check (optional)
-    //   const endOfComment = await driver.findElement(
-    //     webdriver.By.xpath(
-    //       "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div/div[1]/div[4]/div[3]"
-    //     )
-    //   );
-    //   if (endOfComment) break;
-    // } catch (e) {
-    //   continue;
-    // }
+    // if (loadMoreComment){
+    //     loadMoreComment[0].click()
+    //   }
+    // else{break}
+    let comments = await driver.findElements(
+      webdriver.By.className(
+        "_333v"
+      )
+    )
+    
+    try {
+      // thay xpath cần check (optional)
+      if (comments.length < index) break
+    } catch (e) {
+      continue;
+    }
+    index++
   }
 
   // lấy comments
+  // const comments = await driver.findElements(
+  //   webdriver.By.xpath(
+  //     "/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[4]/div/div/div/div[5]/div"
+  //   )
+  // );
   const comments = await driver.findElements(
-    webdriver.By.xpath(
-      "/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[4]/div/div/div/div[5]/div"
+    webdriver.By.className(
+      "_333v"
     )
-  );
+  )
   let commentTxt = "";
   await Promise.all(
     comments.map(async (comment) => {
       const username = await comment
-        .findElement(webdriver.By.xpath("/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[4]/div/div/div/div[5]/div[1]/div[2]/div[1]/div[1]/a"))
-        .getText();
-      const content = await comment
+      
         .findElement(
-          webdriver.By.xpath(
-            "/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[4]/div/div/div/div[5]/div[1]/div[2]/div[1]/div[2]"
+          webdriver.By.className(
+            "_2b05"
           )
         )
         .getText();
+      const content = await comment
+      .findElement(webdriver.By.xpath("/html/body/div[1]/div/div[4]/div/div[1]/div/div/div/div[4]/div/div/div/div[5]/div[1]/div[2]/div[1]/div[2]"))
+        .getText();
 
       commentTxt += `${username}: ${content}\n`;
+      console.log(commentTxt)
     })
   );
   await fs.promises.writeFile(`${Date.now()}.txt`, commentTxt);
